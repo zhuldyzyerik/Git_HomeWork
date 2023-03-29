39b355fbb9fda54a15fec1b873f37d9a89eb1865
zhuldyzyerik <zhuldyz.yerik19@gmail.com>
2023-03-29 00:08:28 -0400

    second commit as a member

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,6 @@ Clone the pushed repo as being a member of a project.
 Update the code and make it print integers till 100.
 Push the code to the same repo
          */
-        for(int i=0;i<=10;i++){
+        for(int i=0;i<=100;i++){
             System.out.print(i+" ");
 }}}
