010475e439b2cb38567269f5a4b5c2e511ba76f9
zhuldyzyerik <zhuldyz.yerik19@gmail.com>
2023-03-28 23:37:44 -0400

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Hw:
+Create a repo on local, have a basic code in it for printing integers from 1 to 10
+Push it to github.
+
+Step2
+Clone the pushed repo as being a member of a project.
+Update the code and make it print integers till 100.
+Push the code to the same repo
+         */
+        for(int i=0;i<=10;i++){
+            System.out.print(i+" ");
+}}}
